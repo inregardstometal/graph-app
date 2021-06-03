04acae4a4c4c97fea5092db8925f66396a2759af
Carson Miller <carsondm@gmail.com>
2021-06-02 22:37:24 -0400

    Adjusting view height to consider toolbar

--- src/Components/GraphVisualizer/GraphVisualizer.tsx
+++ src/Components/GraphVisualizer/GraphVisualizer.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const Graph = styled.div`
     width: 100%;
-    height: 100%;
+    height: calc(100% - 50px);
     border-radius: 10px;
     border: 2px solid white;
     overflow: hidden;
